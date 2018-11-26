471d387ededdffbf7779b5f597479861a71d86ce
roddom67 <roddom67@gmail.com>
2018-11-26 19:49:17 -0300

    publicar

--- calix-barrancas/email_body.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n". $message
-
-?> 
--- template/template-descargar-brochure.php
+++
@@ -1,7 +0,0 @@
-<section class="descargarBrochure">
-	<div class="container-fluid">
-		<article>
-			<a href="brochure/twowinds-urquiza.pdf" title="Descargar brochure" target="_blank">Descargar brochure</a>
-		</article>
-	</div>
-</section>
